800e7dcb395bb8288c2dde0ed856d33c2307f8c1
r-liron <rafael.liron@cpe.fr>
2025-10-02 09:29:03 +0200

    Add files via upload

---
+++ pendu_TP_3.py
@@ -0,0 +1,10 @@
+# fichier crée par Rafael Liron le 02/10/2025
+# créer des fonctions permettant de réaliser un pendu, méthode console puis graphique.
+import random
+
+# def choix():
+#     with open("mots.txt", "r") as file:
+#         texte = file.read()
+#         mots = list(map(str, texte.split()))
+#         choisi = random.choice(mots)
+#     # return choisi
